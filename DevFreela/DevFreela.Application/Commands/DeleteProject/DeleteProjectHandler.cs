8c5915fa5682d1491eea0853e290cee048ff406a
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2026-02-09 23:40:51 -0300

    criando seus primeiros testes com xUnit

---
+++ DevFreela/DevFreela.Application/Commands/DeleteProject/DeleteProjectHandler.cs
@@ -0,0 +1,32 @@
+﻿using DevFreela.Application.Models;
+using DevFreela.Core.Repositories;
+using MediatR;
+
+namespace DevFreela.Application.Commands.DeleteProject
+{
+    public class DeleteProjectHandler : IRequestHandler<DeleteProjectCommand, ResultViewModel>
+    {
+        public const string PROJECT_NOT_FOUND_MESSAGE = "Projeto não existe.";
+
+        private readonly IProjectRepository _repository;
+        public DeleteProjectHandler(IProjectRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ResultViewModel> Handle(DeleteProjectCommand request, CancellationToken cancellationToken)
+        {
+            var project = await _repository.GetById(request.Id);
+
+            if (project is null)
+            {
+                return ResultViewModel.Error(PROJECT_NOT_FOUND_MESSAGE);
+            }
+
+            project.SetAsDeleted();
+            await _repository.Update(project);
+
+            return ResultViewModel.Success();
+        }
+    }
+}
---
+++ DevFreela/DevFreela.UnitTests/Core/ProjectTests.cs
@@ -0,0 +1,37 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Enums;
+
+namespace DevFreela.UnitTests.Core
+{
+    public class ProjectTests
+    {
+        [Fact]
+        public void ProjectIsCreated_Start_Success()
+        {
+            // Arrange
+            var project = new Project("Projeto A", "Descrição do Projeto", 1, 2, 10000);
+
+            // Act
+           project.Start();
+
+            // Assert
+            Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+        }
+
+        [Fact]
+        public void ProjectIsInvalidState_Start_ThrowsException()
+        {
+            // Arrange
+            var project = new Project("Projeto A", "Descrição do Projeto", 1, 2, 10000);
+            project.Start();
+
+            // Act
+            Action? start = project.Start;
+
+            // Assert
+            var exception = Assert.Throws<InvalidOperationException>(start);
+            Assert.Equal(Project.INVALID_STATE_MESSAGE, exception.Message);
+        }
+    }
+}
