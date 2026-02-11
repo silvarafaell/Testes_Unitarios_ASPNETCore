581670fb8d897cedc7f268838206a18cbd339ae0
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2026-02-11 20:04:21 -0300

    mocking com NSubstiture - prática

---
+++ DevFreela/DevFreela.UnitTests/Application/DeleteProjectHandlerTests.cs
@@ -0,0 +1,59 @@
+﻿using DevFreela.Application.Commands.DeleteProject;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using NSubstitute;
+
+namespace DevFreela.UnitTests.Application
+{
+    public class DeleteProjectHandlerTests
+    {
+        [Fact]
+        public async Task ProjectExists_Delete_Success_NSubstitute()
+        {
+            // Arrange
+             var project = new Project("Projeto A", "Descrição de Projeto", 1, 2, 10000);
+
+            var repository = Substitute.For<IProjectRepository>();
+            repository.GetById(1).Returns(Task.FromResult((Project?)project));
+            repository.Update(Arg.Any<Project>()).Returns(Task.CompletedTask);
+
+            var handler = new DeleteProjectHandler(repository);
+
+            var command = new DeleteProjectCommand(1);
+
+            // Act
+            var result = await handler.Handle(command, new CancellationToken());
+
+            // Assert
+            Assert.True(result.IsSuccess);
+            await repository.Received(1).GetById(1);
+            await repository.Received(1).Update(Arg.Any<Project>());
+        }
+
+        [Fact]
+        public async Task ProjectDoesNotExist_Delete_Error_NSubstitute()
+        {
+            // Arrange
+            var repository = Substitute.For<IProjectRepository>();
+            repository.GetById(Arg.Any<int>()).Returns(Task.FromResult((Project?)null));
+
+            var handler = new DeleteProjectHandler(repository);
+
+            var command = new DeleteProjectCommand(1);
+
+            // Act
+            var result = await handler.Handle(command, new CancellationToken());
+
+            // Assert
+            Assert.False(result.IsSuccess);
+
+
+            Assert.Equal(DeleteProjectHandler.PROJECT_NOT_FOUND_MESSAGE, result.Message);
+
+            await repository.Received(1).GetById(Arg.Any<int>());
+            await repository.DidNotReceive().Update(Arg.Any<Project>());
+
+        }
+
+    }
+}
---
+++ DevFreela/DevFreela.UnitTests/Application/InsertProjectHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿using DevFreela.Application.Commands.InsertProject;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using NSubstitute;
+
+namespace DevFreela.UnitTests.Application
+{
+    public class InsertProjectHandlerTests
+    {
+        [Fact]
+        public async Task InputDataAreOk_Insert_Success_NSubstitute()
+        {
+            // Arrange
+            const int ID = 1;
+
+            var repository = Substitute.For<IProjectRepository>();
+            repository.Add(Arg.Any<Project>()).Returns(Task.FromResult(ID));
+
+            var command = new InsertProjectCommand
+            {
+                Title = "Project A",
+                Description = "Descrição do Projeto",
+                TotalCost = 20000,
+                IdClient = 1,
+                IdFreelancer = 2
+            };
+
+            var handler = new InsertProjectHandler(repository);
+
+            // Act
+            var result = await handler.Handle(command, new CancellationToken());
+
+            // Assert
+            Assert.True(result.IsSuccess);
+            Assert.Equal(ID, result.Data);
+            await repository.Received(2).Add(Arg.Any<Project>());
+        }
+    }
+}
