4ede22fa234e2e7720ca3a21473387d853baad8f
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2026-02-12 12:11:34 -0300

    Mocking com Moq - prática

--- DevFreela/DevFreela.UnitTests/Application/DeleteProjectHandlerTests.cs
+++ DevFreela/DevFreela.UnitTests/Application/DeleteProjectHandlerTests.cs
@@ -1,6 +1,7 @@
 ﻿using DevFreela.Application.Commands.DeleteProject;
 using DevFreela.Core.Entities;
 using DevFreela.Core.Repositories;
+using Moq;
 using NSubstitute;
 
 namespace DevFreela.UnitTests.Application
@@ -52,8 +53,56 @@ namespace DevFreela.UnitTests.Application
 
             await repository.Received(1).GetById(Arg.Any<int>());
             await repository.DidNotReceive().Update(Arg.Any<Project>());
-
         }
+
+        [Fact]
+        public async Task ProjectExists_Delete_Success_Moq()
+        {
+            // Arrange
+             var project = new Project("Projeto A", "Descrição de Projeto", 1, 2, 10000);
+
+            var repository = Mock.Of<IProjectRepository>(p =>
+                p.GetById(It.IsAny<int>()) == Task.FromResult(project) &&
+                p.Update(It.IsAny<Project>()) == Task.CompletedTask
+                );
+
+            var handler = new DeleteProjectHandler(repository);
+
+            var command = new DeleteProjectCommand(1);
+
+            // Act
+            var result = await handler.Handle(command, new CancellationToken());
+
+            // Assert
+            Assert.True(result.IsSuccess);
+
+            Mock.Get(repository).Verify(r => r.GetById(1), Times.Once);
+            Mock.Get(repository).Verify(r => r.Update(It.IsAny<Project>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task ProjectDoesNotExist_Delete_Error_Moq()
+        {
+            // Arrange
+            var repository = Mock.Of<IProjectRepository>(r =>
+                r.GetById(It.IsAny<int>()) == Task.FromResult((Project?)null)
+            );
+
+            var handler = new DeleteProjectHandler(repository);
+
+            var command = new DeleteProjectCommand(1);
+
+            // Act
+            var result = await handler.Handle(command, new CancellationToken());
+
+            // Assert
+            Assert.False(result.IsSuccess);
+            Assert.Equal(DeleteProjectHandler.PROJECT_NOT_FOUND_MESSAGE, result.Message);
+
+            Mock.Get(repository).Verify(r => r.GetById(1), Times.Once);
+            Mock.Get(repository).Verify(r => r.Update(It.IsAny<Project>()), Times.Never);
+        }
+
 
     }
 }
--- DevFreela/DevFreela.UnitTests/Application/InsertProjectHandlerTests.cs
+++ DevFreela/DevFreela.UnitTests/Application/InsertProjectHandlerTests.cs
@@ -1,6 +1,7 @@
 ﻿using DevFreela.Application.Commands.InsertProject;
 using DevFreela.Core.Entities;
 using DevFreela.Core.Repositories;
+using Moq;
 using NSubstitute;
 
 namespace DevFreela.UnitTests.Application
@@ -34,6 +35,41 @@ namespace DevFreela.UnitTests.Application
             Assert.True(result.IsSuccess);
             Assert.Equal(ID, result.Data);
             await repository.Received(2).Add(Arg.Any<Project>());
+        }
+
+        [Fact]
+        public async Task InputDataAreOk_Insert_Success_Moq()
+        {
+            // Arrange
+            const int ID = 1;
+
+            //var mock = new Mock<IProjectRepository>();
+            //mock.Setup(r => r.Add(It.IsAny<Project>())).ReturnsAsync(ID);
+
+            var repository = Mock
+                .Of<IProjectRepository>(r => r.Add(It.IsAny<Project>()) == Task.FromResult(ID));
+
+            var command = new InsertProjectCommand
+            {
+                Title = "Project A",
+                Description = "Descrição do Projeto",
+                TotalCost = 20000,
+                IdClient = 1,
+                IdFreelancer = 2
+            };
+
+            var handler = new InsertProjectHandler(repository);
+
+            // Act
+            var result = await handler.Handle(command, new CancellationToken());
+
+            // Assert
+            Assert.True(result.IsSuccess);
+            Assert.Equal(ID, result.Data);
+
+            // mock.Verify(m => m.Add(It.IsAny<Project>()), Times.Once);
+
+            Mock.Get(repository).Verify(m => m.Add(It.IsAny<Project>()), Times.Once);
         }
     }
 }
